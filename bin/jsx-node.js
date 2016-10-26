8f79af2029d264c836f95a468837ee91b3b44873
Linus Miller <lohfu@lohfu.io>
2016-10-26 10:40:31 +0200

    Implement executable to compile jsx

---
+++ bin/jsx-node.js
@@ -0,0 +1,29 @@
+#!/bin/env node
+
+'use strict';
+
+const babel = require('babel-core');
+
+const defaultOptions = {
+  extension: '.jsx',
+  presets: ['es2015-node6'],
+  plugins: [
+    'add-module-exports',
+  ],
+};
+
+// process.exit(0);
+const filename = process.argv[2];
+
+const result = babel.transformFileSync(filename, {
+  presets: defaultOptions.presets,
+  plugins: [
+    ...defaultOptions.plugins,
+    ['transform-react-jsx', { pragma: 'h' }],
+    require('../babel'),
+  ],
+});
+
+process.stdout.write(result.code);
+
+process.exit();
